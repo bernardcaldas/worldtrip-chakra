42ee031ad0b3b6370b36f23f9acfa77a02c32cc7
bernardoDev <bernardo.acaldas@gmail.com>
2021-12-07 21:07:30 -0300

    update

---
+++ world-trip/src/components/Header/index.tsx
@@ -0,0 +1,37 @@
+import { Flex, Grid, Image } from "@chakra-ui/react";
+
+export default function Header(){
+    return(
+        <Flex
+          as ="header"
+          w="100%"
+          bg="white"
+          mx="auto"
+          h="100"
+          align="center"
+          justify="center"
+          >
+
+              <Grid
+                h="100"
+                mx="auto"
+                w="100%"
+                maxWidth="1280"
+                alignItems="center"
+                templateColumns="repeat(3fr, 1)"
+                justifyContent="center">
+
+                    <Image 
+                    src="/LogoTravel.svg"
+                    alt="logo travel logo"
+                    gridColumn="2"
+                    />
+
+
+              </Grid>
+
+
+
+        </Flex>
+    )
+}
--- world-trip/src/pages/index.tsx
+++ world-trip/src/pages/index.tsx
@@ -1,6 +1,11 @@
+import { Flex } from "@chakra-ui/react";
+import Header from '../components/Header';
+
 
 export default function Home() {
   return (
-   <h1>WorlTrip</h1>
+   <Flex flexDirection="column" h="100vh">
+     <Header />
+   </Flex>
   )
 }
