1877fa6251f6bdeb90b0683d76956cdab41e83b7
bernardoDev <bernardo.acaldas@gmail.com>
2021-12-09 21:48:14 -0300

    heading

--- world-trip/src/pages/index.tsx
+++ world-trip/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
+import Divider from "../components/Divider";
 import Header from '../components/Header';
 import Highlighted from "../components/Highlighted";
 import TravelTypes from "../components/TravelTypes";
@@ -10,6 +11,14 @@ export default function Home() {
      <Header />
      <Highlighted />
      <TravelTypes />
+     <Divider />
+     <Heading
+      textAlign="center"
+      fontWeight="500"
+      fontSize={["lg", "3xl", "4xl"]}
+      mb={["5", "14"]}
+     >
+       Vamos nessa ?<br/> Então escolha seu continente</Heading>
 
    </Flex>
   )
